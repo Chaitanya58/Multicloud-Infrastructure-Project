e66ab1462803e3bd5e58c6a1874888f5b517f225
Chaitanya58 <84025612+Chaitanya58@users.noreply.github.com>
2023-10-11 10:01:35 +0530

    Create Vnet.tf
    Terraform Code to create a Virtual Network(Vnet) on AZURE

---
+++ Vnet.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "RG" {
+  name     = "vs-terraform"
+  location = "West Europe"
+
+  tags = {
+    Owner = "Saru"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vs-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "VMsubnet"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
